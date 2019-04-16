0f568e0ee9dc2fae828ee6db59ee31c650e2cbb5
AlexxxxK <49614264+AlexxxxK@users.noreply.github.com>
2019-04-16 20:20:43 +0300

    Update script.js

--- 1/1.3/script.js
+++ 1/1.3/script.js
@@ -7,9 +7,7 @@ const SHIPPING_PRICE_AUSTRALIA = 170;
 const SHIPPING_PRICE_INDIA = 80;
 const SHIPPING_PRICE_JAMAICA = 120;
 
-destinationCountry = prompt("Please enter the country o shipment");
-destinationCountry.toLowerCase();
-
+destinationCountry = prompt("Please enter the country o shipment").toLowerCase();
 switch(destinationCountry) {
     case "china":
         console.log(`Shipment to ${destinationCountry} will cost ${SHIPPING_PRICE_CHINA} credits`);
