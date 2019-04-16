3cdf071206d19f202fc5dca77a06f4a8d996a62c
AlexxxxK <49614264+AlexxxxK@users.noreply.github.com>
2019-04-16 19:58:04 +0300

    Update script.js

--- 1/1.3/script.js
+++ 1/1.3/script.js
@@ -1,33 +1,32 @@
 'use strict'
 
 let destinationCountry;
-let price;
+const SHIPPING_PRICE_CHINA = 100;
+const SHIPPING_PRICE_SOUTH_AMERICA = 250;
+const SHIPPING_PRICE_AUSTRALIA = 170;
+const SHIPPING_PRICE_INDIA = 80;
+const SHIPPING_PRICE_JAMAICA = 120;
 
 destinationCountry = prompt("Please enter the country o shipment");
-destinationCountry = destinationCountry.toLowerCase();
+destinationCountry.toLowerCase();
 
 switch(destinationCountry) {
     case "china":
-        price = 100;
-        alert(`Shipment to ${destinationCountry.toUpperCase()} will cost ${price} credits`);
+        console.log(`Shipment to ${destinationCountry} will cost ${SHIPPING_PRICE_CHINA} credits`);
     break;
     case "south america":
-        price = 250;
-        alert(`Shipment to ${destinationCountry.toUpperCase()} will cost ${price} credits`);
+        console.log(`Shipment to ${destinationCountry} will cost ${SHIPPING_PRICE_SOUTH_AMERICA} credits`);
     break;
     case "australia":
-        price = 170;
-        alert(`Shipment to ${destinationCountry.toUpperCase()} will cost ${price} credits`);
+        console.log(`Shipment to ${destinationCountry} will cost ${SHIPPING_PRICE_AUSTRALIA} credits`);
     break;
     case "india":
-        price = 80;
-        alert(`Shipment to ${destinationCountry.toUpperCase()} will cost ${price} credits`);
+        console.log(`Shipment to ${destinationCountry} will cost ${SHIPPING_PRICE_INDIA} credits`);
     break;
     case "jamaica":
-        price = 120;
-        alert(`Shipment to ${destinationCountry.toUpperCase()} will cost ${price} credits`);
+        console.log(`Shipment to ${destinationCountry} will cost ${SHIPPING_PRICE_JAMAICA} credits`);
     break;
     default:
-        alert(`Shipment is not available for your country`);
+        console.log(`Shipment is not available for your country`);
     break;
 }
