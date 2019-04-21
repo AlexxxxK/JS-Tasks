5859fb7d3658c4a56223229e5419313b3b6e5603
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-04-21 11:16:47 +0300

    Task 2

--- 2/2.1/script.js
+++ 2/2.1/script.js
@@ -6,14 +6,16 @@ let total = 0;
 
 do {
     input = prompt("Введите число");
-    if (isNaN(input) === true) {
+    if (input === null) {
+        break;
+    } else if (Number.isNaN(Number(input))) {
         alert("Было введено не число, попробуйте еще раз");
-    } else if (input !== null) {
+    } else {
         numbers.push(Number(input));
     }
 } while (input !== null);
 
-if (numbers.length > 0) {
+if (numbers.length) {
     for (const value of numbers) {
         total += value;
     }
--- 2/2.2/script.js
+++ 2/2.2/script.js
@@ -3,27 +3,22 @@
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let attemptsLeft = 3;
 let userPasswordInput;
-let isPasswordCorrect = false;
 
 do {
     userPasswordInput = prompt('Введите пароль');
+
     if (userPasswordInput === null) {
         break;
-    } else {
-        for (const value of passwords) {
-            if (userPasswordInput === value) {
-                alert('Добро пожаловать!');
-                isPasswordCorrect = true;
-                break;
-            }
-        }
-        if (isPasswordCorrect === false) {
-            attemptsLeft -= 1;
-        } 
-        if (attemptsLeft > 0 && isPasswordCorrect === false) {
-            alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
-        } else if (attemptsLeft === 0 && isPasswordCorrect === false) {            
-            alert('У вас закончились попытки, аккаунт заблокирован!');
-        }
+    } else if (passwords.includes(userPasswordInput)) {
+        alert('Добро пожаловать!');
+        break;
     }
-} while (attemptsLeft > 0 && isPasswordCorrect === false);
+
+    attemptsLeft -= 1;
+
+    if (attemptsLeft) {
+        alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
+    } else {            
+        alert('У вас закончились попытки, аккаунт заблокирован!');
+    }
+} while (attemptsLeft);
