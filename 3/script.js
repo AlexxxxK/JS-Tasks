c4d0ab827eb97828726274b2976d9e07880363ee
AlexxxxK <49614264+AlexxxxK@users.noreply.github.com>
2019-04-25 20:09:06 +0300

    Update script.js

--- 3/script.js
+++ 3/script.js
@@ -25,6 +25,10 @@ const addLogin = function(allLogins, login) {
 
 // Вызовы функции для проверки
 addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+console.log(addLogin);
 addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+console.log(addLogin);
 addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin);
 addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin);
