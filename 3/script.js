36acf45171a21da73023cae01709026cf0cf01db
AlexxxxK <49614264+AlexxxxK@users.noreply.github.com>
2019-04-25 20:47:32 +0300

    Update script.js

--- 3/script.js
+++ 3/script.js
@@ -11,27 +11,20 @@ const isLoginUnique = function(allLogins, login) {
 };
 
 const addLogin = function(allLogins, login) {
-  let message;
   if (!isLoginValid(login)) {
-    message = 'Ошибка! Логин должен быть от 4 до 16 символов';
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
   } 
   
   if (!isLoginUnique(allLogins, login)) {
-    message = 'Такой логин уже используется!';
+    return = 'Такой логин уже используется!';
   } else {
     allLogins.push(login);
-    message = 'Логин успешно добавлен!';
+    return = 'Логин успешно добавлен!';
   }
-  
-  return message;
 };
 
 // Вызовы функции для проверки
 addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
-console.log(addLogin);
 addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
-console.log(addLogin);
 addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin);
 addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin);
