0f2b9dc8a556e9bfb430c91149db6a3f6c6baf1b
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-06-03 21:52:23 +0300

    Task 08

--- 7/script.js
+++ 7/script.js
@@ -88,35 +88,35 @@ const users = [
   },
 ];
 
-//Получить массив имен всех пользователей (поле name).
+//1 Получить массив имен всех пользователей (поле name).
 const getAllNames = users => users.map(user => user.name);
 
 console.log(getAllNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-//Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+//2 Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 const getUsersByEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
 
 console.log(getUsersByEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-//Получить массив имен пользователей по полу (поле gender).
+//3 Получить массив имен пользователей по полу (поле gender).
 const getUsersByGender = (users, gender) =>
   users.filter(user => user.gender === gender).map(user => user.name);
 
 console.log(getUsersByGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-//Получить массив только неактивных пользователей (поле isActive).
+//4 Получить массив только неактивных пользователей (поле isActive).
 const getInactiveUsers = users => users.filter(user => !user.isActive);
 
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-//Получить пользоваля (не массив) по email (поле email, он уникальный).
+//5 Получить пользоваля (не массив) по email (поле email, он уникальный).
 const getUserByEmail = (users, email) => users.find(user => user.email === email);
 
 console.log(getUserByEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
 console.log(getUserByEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
 
-//Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+//6 Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
 const getUsersWithAge = (users, min, max) =>
   users.filter(user => user.age >= min && user.age <= max);
 
@@ -125,12 +125,12 @@ console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, об
 console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-//Получить общую сумму баланса (поле balance) всех пользователей.
+//7 Получить общую сумму баланса (поле balance) всех пользователей.
 const getTotalBalance = users => users.reduce((acc, curr) => acc + curr.balance, 0);
 
 console.log(getTotalBalance(users)); // 20916
 
-//Массив имен всех пользователей у которых есть друг с указанным именем.
+//8 Массив имен всех пользователей у которых есть друг с указанным именем.
 
 const getUsersByFriend = (users, name) =>
   users.filter(user => user.friends.includes(name)).map(user => user.name);
@@ -138,6 +138,7 @@ const getUsersByFriend = (users, name) =>
 console.log(getUsersByFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersByFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
+//Дополнительное задание
 //Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке
 const getUniqueSkills = users =>
   users
